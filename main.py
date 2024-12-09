ba64cc30e1ba928f0097fb46b2387f1a2c323333
KitKattop666 <diana109ri@gmail.com>
2024-12-09 12:13:56 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,37 +7,55 @@
 #     Программа должна быть нечувствительна к регистру.
 #     Игнорировать пробелы и знаки пунктуации.
 
+
 def isPalindrom(word):
-     word = word.replace(" ", "").lower()
-     return word == word[::-1]
-user_imput = input("Ввод:")
-if isPalindrome(user_input):
-    print("Палиндром")
-else:
-    print("Не палиндром")
+    word = word.replace(" ", "").lower()
+user_imput = input("Введите:")
+if user_imput == user_imput[::-1]:
+    print("Полиндром")   
+else:    
+    print("Не полиндром")
+ Правильное(вроде):
+def isPalindrom(word):
+    word = word.lower().replace(" ", "")
+    return word == word[::-1]:
+
+user_imput = input("Ввод: ")
+print(isPalindrom(user_imput))
     
 
 # Задание №2
-def isPalindrom(word):
-     return word == word[::-1]
-     user_imput = input("Ввод слов:")
-     words user_input.split(',')
-     for word in ["".join(filter(str.isalpha, w)) for w in text.split(" ")]:
-          print("palidrome") if word.lover()[::-1] else print("not palidrome")
-          print("Палидром: ")
+
 # Задание: Написать программу, которая принимает список слов и проверяет, какие из них являются палиндромами.
 # Условия:
-
 #     Слова передаются в виде списка через ввод пользователя.
 #     Программа должна вывести все палиндромы из списка.
+# def isPalindromlist(word)
+#     word = word.lower()replace(" ", "")
+#     return word == word[::-1]
 
-#  def isPalindrom(Word):
-#          word= word.lower
-#     return word_lower 
-#     word_lower =  word_lower[::-1]
+# def check_isPalindrom(wordi):
+#     Palindrom = []
+#     for word in word:
+#         if isPalindromlist(word):
+#             Palindrom.append(word.lower())
+#     peturn Palindrom
 
-# Пример использования:
-#     isPalindromList(["hello", "list", "level"]) # ["level"]
+# user_imput = input("Ввод: ")
+# word_list = [word.strip() for word in user_imput.split(",")]
+# print(check_isPalindrom(wordi_list))
+
+
+# abuyz:
+# n = int(input("Enter the number of elements: "))
+# for i in range(n):
+#     element = input(f"Enter element {i+1}: ")
+#     a.append(element)
+# print("List:", a)
+
+
+   
+
 
 # Задание №3
 
@@ -49,3 +67,30 @@ def isPalindrom(word):
 
 # Пример использования isPalindromString("Madam, Anna went to the civic center") # ["madam", "anna", "civic"]
 # """
+def isPalindromlist(word)
+    word = word.lower()replace(" ", "")
+    return word == word[::-1]
+
+def check_isPalindrom(wordi):
+    Palindrom = []
+    for word in word:
+        if isPalindromlist(word):
+            Palindrom.append(word.lower())
+    peturn Palindrom
+
+user_imput = input("Ввод: ")
+word_list = [word.strip() for word in user_imput.split(",")]
+print(check_isPalindrom(wordi_list))
+def isPalindrom(word):
+    word = word.lower().replace(" ", "")
+    return word == word[::-1]:
+def find_palindromStringe(word):
+    word = text.split()
+    palindromes = set()
+    for word in word:
+        word= ".join(char for char in word if char.isalnum())"
+        if isPalindromeString(word) and word:
+            palindrome.add(word.lower())
+    return list(palindromes)
+    user_input = input("Ввод: ")
+    print(find_palindromes(user_input))
